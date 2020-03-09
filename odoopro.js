b7b07d77d438288668218ed79f7a228f1842fa6b
Manuel Antón <manuel.anton@cosmomedia.es>
2020-03-09 12:57:26 +0100

    Auto-detects employee id. Added menu.

--- odoopro.js
+++ odoopro.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Odoo Pro
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Some Odoo utilities
 // @author       Manantt
 // @match        https://cosmomedia-serdata.odoo.com/*
@@ -22,18 +22,70 @@ var minutos = 0;
 var segundos = 0;
 
 recalcularTiempos();
-(function() {
-    setTimeout(function(){
-        $(".o_hr_attendance_kiosk_mode").click(function(){
-            setTimeout(function(){
-                recalcularTiempos();
-            }, 2000);
-        });
-    }, 2000);
-})();
+
+function autoSetIdEmpleado() {
+	if(window.location.href == "https://cosmomedia-serdata.odoo.com/web#action=566&menu_id=393") {
+		idEmpleado = $("img.img.rounded-circle").attr("src").replace("/web/image?model=hr.employee&field=image_medium&id=", "");;
+		localStorage.setItem('id_empleado_odoo', idEmpleado);
+	}
+}
+
+function semana(){
+	var hoy = new Date();
+	var semanaPasada = new Date;
+    semanaPasada = new Date(semanaPasada.setDate(semanaPasada.getDate() - 7));
+
+	var inicioSemana = getInicioSemana(hoy); //"2020-03-01 22:00:00";
+	var finSemana = getFinSemana(hoy); //"2020-03-08 22:00:00";
+	var payloadSemana = '{"jsonrpc":"2.0","method":"call","params":{"args":[],"model":"hr.attendance","method":"read_group","kwargs":{"context":{"lang":"es_ES","tz":"Europe/Madrid","uid":51,"active_model":"hr.employee","active_id":794,"active_ids":[794],"search_default_employee_id":794,"default_employee_id":794,"search_disable_custom_filters":true,"group_by":"check_in"},"domain":[["employee_id","=",794],["employee_id","=",794],"&",["check_in",">=","'+inicioSemana+'"],["check_in","<","'+finSemana+'"]],"fields":["x_studio_nombre_empresa","x_studio_nif_empresa","x_studio_field_Vq3gL","employee_id","check_in","check_out","worked_hours"],"groupby":["check_in:week"],"orderby":"","lazy":true}},"id":591586260}';
+	$.ajax({
+        url: 'https://cosmomedia-serdata.odoo.com/web/dataset/call_kw/hr.attendance/read_group',
+        type: "POST",
+        dataType: "json",
+        data: payloadSemana,
+        contentType: "application/json",
+        success: function(data){
+            $("#semana").html(toHM(data.result[0].worked_hours));
+        }
+    });
+
+    var inicioSemanaPasada = getInicioSemana(semanaPasada);
+	var finSemanaPasada = getFinSemana(semanaPasada);
+    var payloadSemanaPasada = '{"jsonrpc":"2.0","method":"call","params":{"args":[],"model":"hr.attendance","method":"read_group","kwargs":{"context":{"lang":"es_ES","tz":"Europe/Madrid","uid":51,"active_id":794,"active_ids":[794],"params":{"action":778,"active_id":794,"model":"hr.attendance","view_type":"list","menu_id":377},"search_default_employee_id":794,"default_employee_id":794,"search_disable_custom_filters":true,"group_by":"check_in"},"domain":[["employee_id","=",794],["employee_id","=",794],"&",["check_in",">=","'+inicioSemanaPasada+'"],["check_in","<","'+finSemanaPasada+'"]],"fields":["x_studio_nombre_empresa","x_studio_nif_empresa","x_studio_field_Vq3gL","employee_id","check_in","check_out","worked_hours"],"groupby":["check_in:week"],"orderby":"","lazy":true}},"id":6608434}';
+	$.ajax({
+        url: 'https://cosmomedia-serdata.odoo.com/web/dataset/call_kw/hr.attendance/read_group',
+        type: "POST",
+        dataType: "json",
+        data: payloadSemanaPasada,
+        contentType: "application/json",
+        success: function(data){
+            $("#semanaPasada").html(toHM(data.result[0].worked_hours));
+        }
+    });
+}
+
+function getInicioSemana(fecha) {
+	var day = fecha.getDay();
+    var diff = fecha.getDate() - day + (day == 0 ? -6 : 1);
+  	fecha.setDate(diff);
+  	return (fecha.getYear() + 1900) + "-" + ("0" + (fecha.getMonth() + 1)).slice(-2) + "-" + ("0" + fecha.getDate()).slice(-2) + " 00:00:00";
+}
+
+function getFinSemana(fecha) {
+	var day = fecha.getDay();
+    var diff = fecha.getDate() - day + 7;
+  	fecha.setDate(diff);
+  	return (fecha.getYear() + 1900) + "-" + ("0" + (fecha.getMonth() + 1)).slice(-2) + "-" + ("0" + fecha.getDate()).slice(-2) + " 00:00:00";
+}
+
+function toHM(horas) {
+	var tiempo = "";
+	var horasEnteras = parseInt(horas);
+	var minutos = parseInt((horas - horasEnteras) * 60);
+	return horasEnteras + "h " + minutos + "m";
+}
 
 function recalcularTiempos(){
-    console.error("asdf");
     $.ajax({
         url: 'https://cosmomedia-serdata.odoo.com/web/dataset/search_read',
         type: "POST",
@@ -94,27 +146,45 @@ const esHoy = (fecha) => {
       fecha.getFullYear() == hoy.getFullYear();
 }
 
-// (function() {
-//     setTimeout(function(){
-//         init();
-//     }, 2000);
-// })();
+(function() {
+    setTimeout(function(){
+        init();
+    }, 2000);
+})();
 
-// function init() {
-//     var menuSuperior = $(".o_menu_systray");
-//     var html = `
-//         <li class="o_mail_systray_item o_no_notification">
-//             <a aria-expanded="false" class="dropdown-toggle o-no-caret" data-display="static" data-toggle="dropdown" href="#" role="button" title="Odoo Pro">
-//                 <i aria-label="Odoo Pro" class="fa fa-terminal" role="img"></i> <span class="o_notification_counter badge badge-pill">0</span>
-//             </a>
-//             <div class="o_mail_systray_dropdown dropdown-menu dropdown-menu-right" role="menu">
-//                 <div class="o_mail_systray_dropdown_items">
-//                     <div class="dropdown-item-text text-center o_no_activity">
-//                         <span>Sin actividades planificadas.</span>
-//                     </div>
-//                 </div>
-//             </div>
-//         </li>
-//     `;
-//     menuSuperior.prepend(html.replace(/\n/g, ""));
-// }
+function init() {
+    var menuSuperior = $(".o_menu_systray");
+    var html = `
+        <li class="o_mail_systray_item o_no_notification">
+            <a aria-expanded="false" class="dropdown-toggle o-no-caret" data-display="static" data-toggle="dropdown" href="#" role="button" title="Odoo Pro">
+                <span id="pro" style="font-weight: bold;top: -2px;position:relative;color: rgb(250, 100, 50);">Pro</span> <span class="o_notification_counter badge badge-pill">0</span>
+            </a>
+            <div class="o_mail_systray_dropdown dropdown-menu dropdown-menu-right" role="menu">
+                <div class="o_mail_systray_dropdown_items">
+                    <div class="dropdown-item-text">
+                        <table style="width: 100%;margin:10px;">
+                        	<tr>
+                        		<td><b>Trabajado semana pasada:</b></td>
+                        		<td id="semanaPasada"></td>
+                        	</tr>
+                        	<tr>
+                        		<td><b>Trabajado esta semana</b></td>
+                        		<td id="semana"></td>
+                        	</tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </li>
+    `;
+    menuSuperior.prepend(html.replace(/\n/g, ""));
+    semana();
+    var interval = setInterval(function(){
+    	if(!localStorage.getItem('id_empleado_odoo')) {
+    		autoSetIdEmpleado();
+    	} else {
+    		$("#pro").css("color", "rgb(100, 255, 100);");
+    		clearInterval(interval);
+    	}
+    }, 3000);
+}
